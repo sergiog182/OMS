646063ad17968bf0bb01afe0459ca034a939eb1c
sergio gutierrez <sergioa.gutierrezs@gmail.com>
2017-10-31 21:36:00 -0500

    Formulario de productos completo

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,7 +1,10 @@
 var urlservicios = "http://localhost:8984/solr/productsCore/";
+var imagesToAdd = new Array();
+var imagesToDelete = new Array();
 
-function printProductData(jsonData, headers, canEdit, divName, parametro) {
-	console.log(jsonData);
+function printProductData(jsonData, headers, canEdit, divName, parametro, page) {
+	var divTableContainer = document.createElement("DIV");
+	$(divTableContainer).addClass("table-container");
 	var table = document.createElement("TABLE");
 	$(table).addClass("table table-bordered table-hover");
 	var thead = document.createElement("THEAD");
@@ -78,7 +81,7 @@ function printProductData(jsonData, headers, canEdit, divName, parametro) {
 		$(imageDetail).attr("data-price", element.price);
 		$(imageDetail).attr("data-externalidentifier", element.externalidentifier);
 		$(imageDetail).attr("data-description", element.description);
-		$(imageDetail).attr("data-images", element.images);
+		$(imageDetail).attr("data-images", element.images.join("~separador~"));
 		$(imageDetail).attr("data-id", element.id);
 		$(imageDetail).attr("data-status", element.status);
 		var spanDetail = document.createElement("SPAN");
@@ -99,7 +102,7 @@ function printProductData(jsonData, headers, canEdit, divName, parametro) {
 			$(imageEdit).attr("data-price", element.price);
 			$(imageEdit).attr("data-externalidentifier", element.externalidentifier);
 			$(imageEdit).attr("data-description", element.description);
-			$(imageEdit).attr("data-images", element.images);
+			$(imageEdit).attr("data-images", element.images.join("~separador~"));
 			$(imageEdit).attr("data-id", element.id);
 			$(imageEdit).attr("data-status", element.status);
 			var spanEdit = document.createElement("SPAN");
@@ -118,25 +121,26 @@ function printProductData(jsonData, headers, canEdit, divName, parametro) {
 
 	table.appendChild(thead);
 	table.appendChild(tbody);
+	$(divTableContainer).html(table);
 	var div = document.getElementById(divName);
 	$(div).addClass("active");
-	$(div).html(table);
+	$(div).html(divTableContainer);
 
-	getPaginator(parametro, 1, 10);
+	getPaginator(parametro, page, 10);
 }
 
-function getProducts(start, rows, parametro, divResult)
+function getProducts(start, rows, parametro, divResult, page)
 {
 	$.getJSON(urlservicios + "select?indent=on&q=" + parametro + "&wt=json&sort=id asc&wt=json&rows=" + rows + "&start=" + start, function(result){
 	    var jsonResponse = JSON.parse(JSON.stringify(result));
 	    if (jsonResponse.responseHeader.status == 0) {
 	    	if (jsonResponse.response.numFound > 0) {
-	    		printProductData(jsonResponse.response.docs, ["id", "Nombre", "Descipcion", "Precio", "Identificador Externo", "Productor", "Estado"], true, divResult, parametro);
+	    		printProductData(jsonResponse.response.docs, ["id", "Nombre", "Descipcion", "Precio", "Identificador Externo", "Productor", "Estado"], true, divResult, parametro, page);
 	    	} else {
-	    		return "La consulta no obtuvo resultados";
+	    		$("#result-content").html("La consulta no obtuvo resultados");
 	    	}
 	    } else {
-	    	return "Error realizando consulta";
+	    	$("#result-content").html("Error realizando consulta");
 	    }
   	});
 }
@@ -147,7 +151,7 @@ function getPaginator(parametro, currentPage, itemsToShow)
 	    var jsonResponse = JSON.parse(JSON.stringify(result));
 	    if (jsonResponse.responseHeader.status == 0) {
 	    	if (jsonResponse.response.numFound > 0) {
-	    		printPaginator(currentPage, jsonResponse.response.numFound, itemsToShow, "result-content");
+	    		printPaginator(currentPage, jsonResponse.response.numFound, itemsToShow, "result-content", parametro);
 	    	} else {
 	    		return "La consulta no obtuvo resultados";
 	    	}
@@ -155,86 +159,6 @@ function getPaginator(parametro, currentPage, itemsToShow)
 	    	return "Error realizando consulta";
 	    }
   	});	
-}
-
-function printPaginator(currentPage, totalItems, numberItemsToShow, divPaginator)
-{
-	var table = document.createElement("table");
-	$(table).attr("align", "center")
-	var tr = document.createElement("tr");
-
-	var tdPrevius = document.createElement("td");
-	var btnPrevius = document.createElement("button");
-	$(btnPrevius).addClass("btn-custom btn-sm");
-	var iconPrevius = document.createElement("span");
-	$(iconPrevius).addClass("glyphicon glyphicon-chevron-left");
-	btnPrevius.appendChild(iconPrevius);
-	tdPrevius.appendChild(btnPrevius);
-	tr.appendChild(tdPrevius);
-
-	var cantidad = Math.ceil(totalItems/numberItemsToShow);
-	var contador = 1;
-	if ((currentPage - 2) > 2) {
-		contador = (currentPage - 2);
-		var tdPage = document.createElement("td");
-		var btnPage = document.createElement("button");
-		$(btnPage).addClass("btn-custom btn-sm");
-		var text = document.createTextNode("...");
-		btnPage.appendChild(text);
-		tdPage.appendChild(btnPage);
-		tr.appendChild(tdPage);
-	}
-	var banderaSalir = false;
-	var contadorPaginas = 0;
-	while (cantidad >= contador) {
-		if (contadorPaginas == 5 && (cantidad - contador) > 2) {
-			banderaSalir = true;
-			break;
-		}
-		var tdPage = document.createElement("td");
-		var btnPage = document.createElement("button");
-		$(btnPage).addClass("btn-custom btn-sm");
-		if (contador == currentPage) {
-			$(btnPage).addClass("disabled");
-		}
-		var text = document.createTextNode(contador.toString());
-		btnPage.appendChild(text);
-		tdPage.appendChild(btnPage);
-		tr.appendChild(tdPage);
-		contador++;
-		contadorPaginas++;
-	}
-
-	if (banderaSalir) {
-		var tdPage = document.createElement("td");
-		var btnPage = document.createElement("button");
-		$(btnPage).addClass("btn-custom btn-sm");
-		var text = document.createTextNode("...");
-		btnPage.appendChild(text);
-		tdPage.appendChild(btnPage);
-		tr.appendChild(tdPage);
-
-		var tdPage = document.createElement("td");
-		var btnPage = document.createElement("button");
-		$(btnPage).addClass("btn-custom btn-sm");
-		var text = document.createTextNode(cantidad);
-		btnPage.appendChild(text);
-		tdPage.appendChild(btnPage);
-		tr.appendChild(tdPage);
-	}
-
-	var tdNext = document.createElement("td");
-	var btnNext = document.createElement("button");
-	$(btnNext).addClass("btn-custom btn-sm");
-	var iconNext = document.createElement("span");
-	$(iconNext).addClass("glyphicon glyphicon-chevron-right");
-	btnNext.appendChild(iconNext);
-	tdNext.appendChild(btnNext);
-	tr.appendChild(tdNext);
-	table.appendChild(tr);
-
-	var div = document.getElementById(divPaginator);
-	div.appendChild(table);
 }
 
 $(document).ready(function(){
@@ -248,24 +172,25 @@ $(document).ready(function(){
 		var nombre = $("#nombreProducto").val();
 		if (nombre != "") {
 			if (parametro != "*:*") {
-				parametro += " and name:" + nombre;
+				parametro += " and name:*" + nombre + "*";
 			} else {
-				parametro = "name:" + nombre;	
+				parametro = "name:*" + nombre + "*";	
 			}
 		}
 
 		var descripcion = $("#descripcionProducto").val();
 		if (descripcion != "") {
 			if (parametro != "*:*") {
-				parametro += " and description:" + descripcion;
+				parametro += " and description:*" + descripcion + "*";
 			} else {
-				parametro = "description:" + descripcion;	
+				parametro = "description:*" + descripcion + "*";	
 			}
 		}
-		getProducts(0, 10, parametro, "result-content");
+		getProducts(0, 10, parametro, "result-content", 0);
 	});
 
 	$(document).on("click", ".detailProduct", function(){
+		$("#productCodeLabel").html($(this).data("id"));
 		$("#divName").html($(this).data("name"));
 		$("#divProducer").html($(this).data("producer"));
 		$("#divPrice").html(($(this).data("price")).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
@@ -273,7 +198,7 @@ $(document).ready(function(){
 		$("#divStatus").html($(this).data("status"));
 		$("#divDescription").html($(this).data("description"));
 		
-		var imagenes = ($(this).data("images")).split(",");
+		var imagenes = ($(this).data("images")).split("~separador~");
 		var contador = 0;
 		var clase = "image active-image";
 		$("#productImages").html("");
@@ -287,20 +212,30 @@ $(document).ready(function(){
 		$("#productImages").data("actual", 1);
 		$("#productImages").data("max", contador);
 		$("#productImages").data("min", 1);
+		$("#agregarImagen").removeClass("active-fila");
 		$("#eliminarImagen").removeClass("active-fila");
 
 		$("#btnGuardarProducto").addClass("btn-hide");
 	});
 
 	$(document).on("click", ".editProduct", function(){
+		$("#txtIdProdcut").val($(this).data("id"));
+		$("#productCodeLabel").html($(this).data("id"));
 		$("#divName").html("<input type='text' class='form-control' id='txtName' name='txtName' value='" + $(this).data("name") + "'/>");
-		$("#divProducer").html("<input type='text' class='form-control' id='txtProducer' name='txtProducer' value='" + $(this).data("producer") + "'/>");
+		$("#divProducer").html("<input type='text' class='form-control' id='txtProducer' name='txtProducer' />");
 		$("#divPrice").html("<input type='text' class='form-control' id='txtPrice' name='txtPrice' value='" + $(this).data("price") + "'/>");
 		$("#divExternalIdentifier").html("<input type='text' class='form-control' id='txtExternalIdentifier' name='txtExternalIdentifier' value='" + $(this).data("externalidentifier") + "'/>");
-		$("#divStatus").html("<input type='text' class='form-control' id='txtStatus' name='txtStatus' value='" + $(this).data("status") + "'/>");
+		var selectActivo = "";
+		var selectInactivo = "";
+		if ($(this).data("status").toLowerCase() == "activo") {
+			selectActivo = "selected";
+		} else {
+			selectInactivo = "selected";
+		}
+		$("#divStatus").html("<select class='form-control' id='txtStatus' name='txtStatus'><option value='1' " + selectActivo + ">Activo</option><option value='2' " + selectInactivo + ">Inactivo</option></select>");
 		$("#divDescription").html("<textarea class='form-control' rows='5' id='txtDescription' name='txtDescription'>" + $(this).data("description") + "</textarea>");
 		
-		var imagenes = ($(this).data("images")).split(",");
+		var imagenes = ($(this).data("images")).split("~separador~");
 		var contador = 0;
 		var clase = "image active-image";
 		$("#productImages").html("");
@@ -309,53 +244,198 @@ $(document).ready(function(){
 			if (contador != 1) {
 				clase = "image";
 			}
-			$("#productImages").append("<div class='" + clase + "' id='image" + contador + "'><img src='" + element + "' /> </div>");
+			$("#productImages").append("<div class='" + clase + "' id='image" + contador + "'><img class='img-thumbnail' src='" + element + "' /> </div>");
 		});
 		$("#productImages").data("actual", 1);
 		$("#productImages").data("max", contador);
 		$("#productImages").data("min", 1);
+		$("#agregarImagen").addClass("active-fila");
 		$("#eliminarImagen").addClass("active-fila");
 
+		imagesToAdd[$("#txtIdProdcut").val()] = new Array();
+		imagesToDelete[$("#txtIdProdcut").val()] = new Array();
+
 		$("#btnGuardarProducto").removeClass("btn-hide");
+
+		$("#txtProducer").typeahead({
+	  		source: [
+	  			{id: 1, name: "Cartceivridge"},
+	  			{id: 2, name: "Tweetwoofra"},
+	  			{id: 3, name: "Relifigaar"},
+	  			{id: 4, name: "Mictectplscope"},
+	  			{id: 5, name: "Playjector"},
+	  			{id: 6, name: "Subtopentor"},
+	  			{id: 7, name: "Comceivicscope"},
+	  			{id: 8, name: "Cabculplphone"},
+	  			{id: 9, name: "Bibandor"},
+	  			{id: 10, name: "Comleommridge"},
+	  			{id: 11, name: "Montaor"}
+		  	],
+		  	autoSelect: true
+		});
+
+		$("#txtProducer").val($(this).data("producer")).typeahead("lookup");
+
+	});
+
+	$(document).on("click", "#addProduct", function(){
+		$("#txtIdProdcut").val(0);
+		$("#productCodeLabel").html("");
+		$("#divName").html("<input type='text' class='form-control' id='txtName' name='txtName' />");
+		$("#divProducer").html("<input type='text' data-provide='typeahead' autocomplete='off' class='form-control' id='txtProducer' name='txtProducer'/>");
+		$("#divPrice").html("<input type='text' class='form-control' id='txtPrice' name='txtPrice'/>");
+		$("#divExternalIdentifier").html("<input type='text' class='form-control' id='txtExternalIdentifier' name='txtExternalIdentifier'/>");
+		$("#divStatus").html("<select class='form-control' id='txtStatus' name='txtStatus'><option value='1'>Activo</option><option value='2'>Inactivo</option></select>");
+		$("#divDescription").html("<textarea class='form-control' rows='5' id='txtDescription' name='txtDescription'></textarea>");
+		
+		$("#productImages").data("actual", 0);
+		$("#productImages").data("max", 0);
+		$("#productImages").data("min", 0);
+		$("#agregarImagen").addClass("active-fila");
+		$("#eliminarImagen").addClass("active-fila");
+
+		$("#productImages").html("");
+
+		imagesToAdd[$("#txtIdProdcut").val()] = new Array();
+		imagesToDelete[$("#txtIdProdcut").val()] = new Array();
+
+		$("#btnGuardarProducto").removeClass("btn-hide");
+		$("#txtProducer").typeahead({
+	  		source: [
+	  			{id: 1, name: "Cartceivridge"},
+	  			{id: 2, name: "Tweetwoofra"},
+	  			{id: 3, name: "Relifigaar"},
+	  			{id: 4, name: "Mictectplscope"},
+	  			{id: 5, name: "Playjector"},
+	  			{id: 6, name: "Subtopentor"},
+	  			{id: 7, name: "Comceivicscope"},
+	  			{id: 8, name: "Cabculplphone"},
+	  			{id: 9, name: "Bibandor"},
+	  			{id: 10, name: "Comleommridge"}
+		  	],
+		  	autoSelect: true
+		});
 	});
 
 	$(document).on("click", "#btnGuardarProducto", function(){
-		alert("si");
+		var name = $("#txtName").val();
+		var producer = $('#txtProducer').typeahead("getActive").id;
+		var precio = $("#txtPrice").val();
+		var externalidentifier = $("#txtExternalIdentifier").val();
+		var estado = $("#txtStatus").val();
+		var descripcion = $("#txtDescription").val();
+		var codigo = $("#txtIdProdcut").val();
+		
+		if (name == "" || producer == "" || precio == "" || externalidentifier == "" || estado == "" || descripcion == "") {
+			alert("Ningun campo debe estar vacio");
+		} else {
+			alert("se fue");
+		}
+
 	});
 
 	$(document).on("click", "#image-to-left", function(){
 		var actual = $("#productImages").data("actual");
 		var max = $("#productImages").data("max");
 		var min = $("#productImages").data("min");
+		if (max > 0) {
+			$("#image" + actual).removeClass("active-image");
 
-		$("#image" + actual).removeClass("active-image");
-
-		if ((actual - 1) < min) 
-		{
-			actual = max;
-		} else {
-			actual = actual - 1;
+			if ((actual - 1) < min) 
+			{
+				actual = max;
+			} else {
+				actual = actual - 1;
+			}
+			
+			$("#image" + actual).addClass("active-image");
+			$("#productImages").data("actual", actual);
 		}
-		
-		$("#image" + actual).addClass("active-image");
-		$("#productImages").data("actual", actual);
 	});
 
 	$(document).on("click", "#image-to-right", function(){
 		var actual = $("#productImages").data("actual");
 		var max = $("#productImages").data("max");
 		var min = $("#productImages").data("min");
-		
-		$("#image" + actual).removeClass("active-image");
+		if (max > 0) {
+			$("#image" + actual).removeClass("active-image");
 
-		if ((actual + 1) > max) 
-		{
-			actual = min;
-		} else {
-			actual = actual + 1;
+			if ((actual + 1) > max) 
+			{
+				actual = min;
+			} else {
+				actual = actual + 1;
+			}
+
+			$("#image" + actual).addClass("active-image");
+			$("#productImages").data("actual", actual);
 		}
+	});
 
-		$("#image" + actual).addClass("active-image");
-		$("#productImages").data("actual", actual);
+	$(document).on("click", "#btnImageDelete", function(){
+		var cantidad = $("#productImages").data("max");
+		if (cantidad > 0) {
+			if (confirm("¿Esta seguro de eliminar esta imagen?")) {	
+				var actual = $("#productImages").data("actual");
+				var minima = $("#productImages").data("min");
+				if (imagesToAdd[$("#txtIdProdcut").val()][actual] != null && imagesToAdd[$("#txtIdProdcut").val()][actual] != undefined) {
+					imagesToAdd[$("#txtIdProdcut").val()].splice(actual, 1);
+				} else {
+					imagesToDelete[$("#txtIdProdcut").val()].push($('#image' + actual + ' > img').attr("src"));
+				}
+
+				$("#image" + actual).remove();
+				var primero = true;
+
+				actual++;
+				if (actual <= cantidad) {
+					while (actual <= cantidad) {
+						$("#image" + actual).attr("id", "image" + (actual - 1));
+						if (primero) {
+							$("#image" + (actual - 1)).addClass("active-image");
+							primero = false;
+						}
+						if (imagesToAdd[$("#txtIdProdcut").val()][actual] != null && imagesToAdd[$("#txtIdProdcut").val()][actual] != undefined) {
+							imagesToAdd[$("#txtIdProdcut").val()][(actual - 1)] = imagesToAdd[$("#txtIdProdcut").val()][actual];
+							imagesToAdd[$("#txtIdProdcut").val()].splice(actual, 1);
+						}
+						actual++;
+					}
+				} else {
+					$("#image" + minima).addClass("active-image");
+				}
+
+				$("#productImages").data("max", (cantidad - 1));
+			}
+		}
+	});
+
+	$(document).on("click", "#btnImageAdd", function(){
+		var input = document.getElementById("imgInp");
+		if (input.files && input.files[0]) {
+	        var reader = new FileReader();
+	        var cantidad = $("#productImages").data("max");
+	        reader.onload = function (e) {
+	        	if (cantidad > 0) {
+	        		$(".image").removeClass("active-image");
+	        	}
+	        	cantidad++;
+	        	imagesToAdd[$("#txtIdProdcut").val()][cantidad] = e.target.result;
+	            $("#productImages").append("<div class='image active-image' id='image" + cantidad + "'><img class='img-thumbnail' src='" + e.target.result + "' /> </div>");
+	            $("#productImages").data("actual", cantidad);
+				$("#productImages").data("max", cantidad);
+				$("#productImages").data("min", 1);
+	        }
+
+	        reader.readAsDataURL(input.files[0]);
+
+	        $(input).val("");
+	    }
+	});
+
+	$(document).on("click", ".btn-paginator, #previusPage, #nextPage", function(){
+		var page = $(this).data("page");
+		var parametro = $(this).data("parametro");
+		getProducts(((page * 10) -10), 10, parametro, "result-content", page);
 	});
 });
---
+++ assets/js/utilities.js
@@ -0,0 +1,99 @@
+function printPaginator(currentPage, totalItems, numberItemsToShow, divPaginator, parametro)
+{
+	var table = document.createElement("table");
+	$(table).attr("align", "center")
+	var tr = document.createElement("tr");
+
+	var tdPrevius = document.createElement("td");
+	var btnPrevius = document.createElement("button");
+	$(btnPrevius).addClass("btn-custom btn-sm btn-paginator");
+	$(btnPrevius).data("page", (currentPage - 1));
+	$(btnPrevius).data("parametro", parametro);
+	var iconPrevius = document.createElement("span");
+	$(iconPrevius).addClass("glyphicon glyphicon-chevron-left");
+	btnPrevius.appendChild(iconPrevius);
+	tdPrevius.appendChild(btnPrevius);
+	tr.appendChild(tdPrevius);
+
+	var cantidad = Math.ceil(totalItems/numberItemsToShow);
+	var contador = 1;
+	if ((currentPage - 2) > 2) {
+		contador = (currentPage - 2);
+		var tdPage = document.createElement("td");
+		var btnPage = document.createElement("button");
+		$(btnPage).addClass("btn-custom btn-sm btn-paginator");
+		$(btnPage).data("page", 1);
+		$(btnPage).data("parametro", parametro);
+		$(btnPage).attr("type", "button");
+		var text = document.createTextNode("1");
+		btnPage.appendChild(text);
+		tdPage.appendChild(btnPage);
+		tr.appendChild(tdPage);
+
+		var tdPage = document.createElement("td");
+		var btnPage = document.createElement("button");
+		$(btnPage).addClass("btn-custom btn-sm");
+		var text = document.createTextNode("...");
+		btnPage.appendChild(text);
+		tdPage.appendChild(btnPage);
+		tr.appendChild(tdPage);
+	}
+	var banderaSalir = false;
+	var contadorPaginas = 0;
+	while (cantidad >= contador) {
+		if (contadorPaginas == 5 && (cantidad - contador) > 2) {
+			banderaSalir = true;
+			break;
+		}
+		var tdPage = document.createElement("td");
+		var btnPage = document.createElement("button");
+		$(btnPage).addClass("btn-custom btn-sm btn-paginator");
+		if (contador == currentPage) {
+			$(btnPage).addClass("disabled");
+		}
+		var text = document.createTextNode(contador.toString());
+		$(btnPage).data("page", contador);
+		$(btnPage).data("parametro", parametro);
+		$(btnPage).attr("type", "button");
+		btnPage.appendChild(text);
+		tdPage.appendChild(btnPage);
+		tr.appendChild(tdPage);
+		contador++;
+		contadorPaginas++;
+	}
+
+	if (banderaSalir) {
+		var tdPage = document.createElement("td");
+		var btnPage = document.createElement("button");
+		$(btnPage).addClass("btn-custom btn-sm");
+		var text = document.createTextNode("...");
+		btnPage.appendChild(text);
+		tdPage.appendChild(btnPage);
+		tr.appendChild(tdPage);
+
+		var tdPage = document.createElement("td");
+		var btnPage = document.createElement("button");
+		$(btnPage).addClass("btn-custom btn-sm btn-paginator");
+		$(btnPage).data("page", cantidad);
+		$(btnPage).data("parametro", parametro);
+		var text = document.createTextNode(cantidad);
+		btnPage.appendChild(text);
+		tdPage.appendChild(btnPage);
+		tr.appendChild(tdPage);
+	}
+
+	var tdNext = document.createElement("td");
+	var btnNext = document.createElement("button");
+	$(btnNext).addClass("btn-custom btn-sm btn-paginator");
+	$(btnNext).data("page", (currentPage + 1));
+	$(btnNext).data("parametro", parametro);
+	var iconNext = document.createElement("span");
+	$(iconNext).addClass("glyphicon glyphicon-chevron-right");
+	btnNext.appendChild(iconNext);
+	tdNext.appendChild(btnNext);
+	tr.appendChild(tdNext);
+	table.appendChild(tr);
+
+	var div = document.getElementById(divPaginator);
+	div.appendChild(table);
+}
